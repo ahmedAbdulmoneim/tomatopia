6f56084aeea98a67accb49cf91fcf3b8cbbc923b
Ahmed <3lbashmohands@gmail.com>
2024-02-17 23:57:48 +0200

    create splash screen

---
+++ lib/onboarding/splach.dart
@@ -0,0 +1,44 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'onboarding.dart';
+
+class SplachScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(
+        children: [
+          Image.asset('assets/tomato3.png'),
+          ColorizeAnimatedTextKit(
+            colors: const [
+              Colors.red,
+              Colors.red,
+              Colors.yellow,
+              Colors.lightGreenAccent,
+              Color(0xFF4a8d3d),
+              Color(0xFF4a8d3d),
+              Colors.green
+            ],
+            totalRepeatCount: 1,
+            speed: Duration(milliseconds: 80),
+            text: const ['WELCOME TO ', 'TOMATOPIA', 'TOMATOPIA'],
+            textStyle: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent),
+          ),
+        ],
+      ),
+      nextScreen: OnBoarding(),
+      splashIconSize: 250,
+      curve: Curves.fastLinearToSlowEaseIn,
+      pageTransitionType: PageTransitionType.bottomToTop,
+      splashTransition: SplashTransition.decoratedBoxTransition,
+      animationDuration: const Duration(milliseconds: 1000),
+      duration: 3500,
+    );
+  }
+}
