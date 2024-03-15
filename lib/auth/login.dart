9bc6cf265394f7f7bf53e77784e66e4c42951d10
Ahmed <3lbashmohands@gmail.com>
2024-03-16 00:36:37 +0200

    use login and register methods

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,6 +1,13 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tomatopia/api_services/tomatopia_services.dart';
 import 'package:tomatopia/auth/sign_up.dart';
+import 'package:tomatopia/constant/endpints.dart';
+import 'package:tomatopia/cubit/auth_cubit/login/login_cubit.dart';
 
+import '../cubit/auth_cubit/login/login_states.dart';
 import '../custom_widget/custom_button.dart';
 import '../custom_widget/text_form_filed.dart';
 import '../screens/home.dart';
@@ -9,110 +16,145 @@ class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
 
   final GlobalKey<FormState> formKey = GlobalKey();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Form(
-        key: formKey,
-        child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Stack(
-                      alignment: Alignment.bottomLeft,
-                      children: [
-                        Image.asset(
-                          'assets/tomato1.png',
-                        ),
-                        const Text(
-                          'Login',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green,
-                              fontSize: 25),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    textFormField(
-                        onSaved: (value) {},
-                        label: 'Email address',
-                        keyboardType: TextInputType.emailAddress,
-                        prefix: Icons.alternate_email,
-                        validate: (value) {
-                          if (value.isEmpty) {
-                            return "email can't be empty";
-                          }
-                        }),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    textFormField(
-                        onSaved: (value) {},
-                        label: 'Password',
-                        obscureText: true,
-                        keyboardType: TextInputType.visiblePassword,
-                        prefix: Icons.password,
-                        suffix: Icons.visibility_off_outlined,
-                        validate: (password) {
-                          if (password.toString().isEmpty) {
-                            return "password can't be empty";
-                          }
-                        }),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Forget Password',
-                        style: TextStyle(color: Colors.green),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    customButton(
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Home(),
-                                ));
-                          }
-                        },
-                        text: 'LOGIN'),
-                    Row(
-                      children: [
-                        const Text("Don't have an account "),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => RegisterPage(),
-                                  ));
-                            },
-                            child: const Text(
-                              'SIGN UP',
-                              style: TextStyle(
-                                color: Colors.green,
+    return BlocProvider(
+      create: (context) => LoginCubit(TomatopiaServices(Dio())),
+      child: BlocConsumer<LoginCubit, LoginStates>(
+        listener: (context, state) {
+          if(state is LoginSuccessState){
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Home(),
+                ));
+          }
+        },
+        builder: (context, state) {
+          return SafeArea(
+            child: Form(
+              key: formKey,
+              child: Scaffold(
+                body: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListView(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Stack(
+                            alignment: Alignment.bottomLeft,
+                            children: [
+                              Image.asset(
+                                'assets/tomato1.png',
                               ),
-                            ))
-                      ],
-                    )
-                  ],
+                              const Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                    fontSize: 25),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          textFormField(
+                              controller: emailController,
+                              label: 'Email address',
+                              keyboardType: TextInputType.emailAddress,
+                              prefix: Icons.alternate_email,
+                              validate: (value) {
+                                if (value.isEmpty) {
+                                  return "email can't be empty";
+                                }
+                              }),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          textFormField(
+                              controller: passwordController,
+                              onSaved: (value) {
+                                if (formKey.currentState!.validate()) {
+                                  BlocProvider.of<LoginCubit>(context).login(
+                                      endPoint: loginEndpoint,
+                                      data: {
+                                        'email': emailController.text,
+                                        'password': passwordController.text
+                                      });
+                                }
+                              },
+                              label: 'Password',
+                              obscureText: true,
+                              keyboardType: TextInputType.visiblePassword,
+                              prefix: Icons.password,
+                              suffix: Icons.visibility_off_outlined,
+                              validate: (password) {
+                                if (password.toString().isEmpty) {
+                                  return "password can't be empty";
+                                }
+                              }),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forget Password',
+                              style: TextStyle(color: Colors.green),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          ConditionalBuilder(
+                            condition: state is! LoginLoadingState,
+                            builder: (context) => customButton(
+                                onPressed: () async {
+                                  if (formKey.currentState!.validate()) {
+                                    await BlocProvider.of<LoginCubit>(context).login(
+                                        endPoint: loginEndpoint,
+                                        data: {
+                                          'email': emailController.text,
+                                          'password': passwordController.text
+                                        });
+
+                                  }
+                                },
+                                text: 'LOGIN'),
+                            fallback: (context) => const Center(
+                                child: CircularProgressIndicator(color: Colors.green,)),
+                          ),
+                          Row(
+                            children: [
+                              const Text("Don't have an account "),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => RegisterPage(),
+                                        ));
+                                  },
+                                  child: const Text(
+                                    'SIGN UP',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                    ),
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
--- lib/auth/sign_up.dart
+++ lib/auth/sign_up.dart
@@ -1,5 +1,12 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tomatopia/api_services/tomatopia_services.dart';
+import 'package:tomatopia/constant/endpints.dart';
+import 'package:tomatopia/cubit/auth_cubit/register/register_cubit.dart';
 
+import '../cubit/auth_cubit/register/register_states.dart';
 import '../custom_widget/custom_button.dart';
 import '../custom_widget/text_form_filed.dart';
 import '../screens/home.dart';
@@ -8,133 +15,183 @@ import 'login.dart';
 class RegisterPage extends StatelessWidget {
   RegisterPage({Key? key}) : super(key: key);
   final GlobalKey<FormState> formKey = GlobalKey();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Form(
-        key: formKey,
-        child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Stack(
-                      alignment: Alignment.bottomLeft,
-                      children: [
-                        Image.asset(
-                          'assets/tomato4.png',
-                        ),
-                        const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green,
-                              fontSize: 25),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    textFormField(
-                        validate: (value) {
-                          if (value.isEmpty) {
-                            return "Name is required";
-                          }
-                        },
-                        prefix: Icons.person,
-                        label: 'Full Name ',
-                        keyboardType: TextInputType.name,
-                        onSaved: (value) {}),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    textFormField(
-                      validate: (value) {
-                        if (value.isEmpty) {
-                          return "Email is required";
-                        }
-                      },
-                      onSaved: (value) {},
-                      prefix: Icons.alternate_email_rounded,
-                      label: 'Email address',
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    textFormField(
-                      validate: (value) {
-                        if (value.isEmpty) {
-                          return "Password is required";
-                        }
-                      },
-                      onSaved: (value) {},
-                      prefix: Icons.password,
-                      suffix: Icons.visibility_off_outlined,
-                      label: 'Password ',
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    textFormField(
-                      validate: (value) {
-                        if (value.isEmpty) {
-                          return "Password is required";
-                        }
-                      },
-                      onSaved: (value) {},
-                      prefix: Icons.password,
-                      suffix: Icons.visibility_off_outlined,
-                      label: 'Confirm Password',
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    customButton(
-                        text: 'SIGN UP',
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Home(),
-                                ));
-                          }
-                        }),
-                    Row(
-                      children: [
-                        const Text("Already  have an account "),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => LoginPage(),
-                                  ));
+    return BlocProvider(
+      create: (context) => RegisterCubit(TomatopiaServices(Dio())),
+      child: BlocConsumer<RegisterCubit, RegisterStates>(
+        listener: (context, state) {
+          if (state is RegisterSuccessState) {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Home(),
+                ));
+          }
+        },
+        builder: (context, state) {
+          return SafeArea(
+            child: Form(
+              key: formKey,
+              child: Scaffold(
+                body: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListView(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Stack(
+                            alignment: Alignment.bottomLeft,
+                            children: [
+                              Image.asset(
+                                'assets/tomato4.png',
+                              ),
+                              const Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                    fontSize: 25),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          textFormField(
+                              validate: (value) {
+                                if (value.isEmpty) {
+                                  return "Name is required";
+                                }
+                              },
+                              controller: nameController,
+                              prefix: Icons.person,
+                              label: 'Full Name ',
+                              keyboardType: TextInputType.name,
+                              onSaved: (value) {}),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          textFormField(
+                            controller: emailController,
+                            validate: (value) {
+                              if (value.isEmpty) {
+                                return "Email is required";
+                              }
                             },
-                            child: const Text(
-                              'LOGIN',
-                              style: TextStyle(
+                            onSaved: (value) {},
+                            prefix: Icons.alternate_email_rounded,
+                            label: 'Email address',
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          textFormField(
+                            controller: passwordController,
+                            validate: (value) {
+                              if (value.isEmpty) {
+                                return "Password is required";
+                              }
+                            },
+                            onSaved: (value) {},
+                            prefix: Icons.password,
+                            suffix: Icons.visibility_off_outlined,
+                            label: 'Password ',
+                            obscureText: true,
+                            keyboardType: TextInputType.visiblePassword,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          textFormField(
+                            controller: confirmPasswordController,
+                            validate: (value) {
+                              if (value.isEmpty) {
+                                return "Password is required";
+                              }
+                            },
+                            onSaved: (value) {
+                              if (formKey.currentState!.validate()) {
+                                BlocProvider.of<RegisterCubit>(context)
+                                    .register(
+                                        endPoint: registerEndpoint,
+                                        data: {
+                                      'fullName': nameController.text,
+                                      'email': emailController.text,
+                                      'password': passwordController.text,
+                                      'confirmPassword':
+                                          confirmPasswordController.text,
+                                    });
+                              }
+                            },
+                            prefix: Icons.password,
+                            suffix: Icons.visibility_off_outlined,
+                            label: 'Confirm Password',
+                            obscureText: true,
+                            keyboardType: TextInputType.visiblePassword,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          ConditionalBuilder(
+                            condition: state is! RegisterLoadingState,
+                            builder: (context) => customButton(
+                                text: 'SIGN UP',
+                                onPressed: () {
+                                  if (formKey.currentState!.validate()) {
+                                    BlocProvider.of<RegisterCubit>(context)
+                                        .register(
+                                            endPoint: registerEndpoint,
+                                            data: {
+                                          'fullName': nameController.text,
+                                          'email': emailController.text,
+                                          'password': passwordController.text,
+                                          'confirmPassword':
+                                              confirmPasswordController.text,
+                                        });
+                                  }
+                                }),
+                            fallback: (context) => const Center(
+                              child: CircularProgressIndicator(
                                 color: Colors.green,
                               ),
-                            ))
-                      ],
-                    )
-                  ],
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              const Text("Already  have an account "),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => LoginPage(),
+                                        ));
+                                  },
+                                  child: const Text(
+                                    'LOGIN',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                    ),
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
