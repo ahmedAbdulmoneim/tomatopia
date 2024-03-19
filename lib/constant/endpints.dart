c5fb4f82561677e256b39e45f493b5bd98112f8f
Ahmed <3lbashmohands@gmail.com>
2024-03-20 00:48:15 +0200

    create change password ui

--- lib/constant/endpints.dart
+++ lib/constant/endpints.dart
@@ -1,2 +1,3 @@
 const String loginEndpoint  = 'Account/longin';
 const String registerEndpoint  = 'Account/register';
+const String changePassword = 'Account/CheangePassword';
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tomatopia/constant/validate_password.dart';
 import 'package:tomatopia/custom_widget/custom_button.dart';
 import 'package:tomatopia/custom_widget/text_form_filed.dart';
 
@@ -14,6 +15,7 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   GlobalKey<FormState> formKey = GlobalKey();
+  GlobalKey<FormState> bottomSheetFormKey = GlobalKey();
   TextEditingController? textEditingController;
   File? selectedImage;
   String? name;
@@ -24,115 +26,235 @@ class _ProfileState extends State<Profile> {
       child: Form(
         key: formKey,
         child: Scaffold(
-          body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      selectedImage != null
-                          ? CircleAvatar(
-                              radius: 90,
-                              backgroundImage: FileImage(selectedImage!))
-                          : const CircleAvatar(
-                              radius: 90,
-                              backgroundImage:
-                                  AssetImage('assets/no_profile_image.png')),
-                      IconButton(
+          appBar: AppBar(
+            title: const Text(
+              'Profile',
+            ),
+            centerTitle: true,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        selectedImage != null
+                            ? CircleAvatar(
+                                radius: 90,
+                                backgroundImage: FileImage(selectedImage!))
+                            : const CircleAvatar(
+                                radius: 90,
+                                backgroundImage:
+                                    AssetImage('assets/no_profile_image.png')),
+                        IconButton(
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) => SizedBox(
+                                height: 100,
+                                child: Row(
+                                  children: [
+                                    const Spacer(),
+                                    Column(
+                                      children: [
+                                        IconButton(
+                                          onPressed: () async {
+                                            await takeProfileImage();
+                                            Navigator.pop(context);
+                                          },
+                                          icon: const Icon(
+                                            Icons.camera_alt_outlined,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                        const Text('camera')
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                    Column(
+                                      children: [
+                                        IconButton(
+                                          onPressed: () async {
+                                            await loadProfileImage();
+                                            Navigator.pop(context);
+                                          },
+                                          icon: const Icon(
+                                            Icons.image,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                        const Text('camera')
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.add_a_photo_sharp),
+                          color: Colors.black,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      name != null ? '$name' : 'name',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    textFormField(
+                      prefix: Icons.person,
+                      label: 'change name',
+                      onSaved: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
+                      validate: (value) {
+                        if (value.toString().isEmpty) {
+                          return 'please enter valid name ';
+                        }
+                      },
+                      keyboardType: TextInputType.text,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    customButton(
+                        text: 'Change name',
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            formKey.currentState!.save();
+                          }
+                        },
+                        width: double.infinity),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    customButton(
+                        text: 'Change password',
                         onPressed: () {
                           showModalBottomSheet(
+                            isScrollControlled: true,
                             context: context,
-                            builder: (context) => SizedBox(
-                              height: 100,
-                              child: Row(
-                                children: [
-                                  const Spacer(),
-                                  Column(
-                                    children: [
-                                      IconButton(
-                                        onPressed: () async {
-                                          await takeProfileImage();
-                                          Navigator.pop(context);
-                                        },
-                                        icon: const Icon(
-                                          Icons.camera_alt_outlined,
-                                          color: Colors.blue,
+                            builder: (context) => Form(
+                              key: bottomSheetFormKey,
+                              child: Container(
+                                height: 350,
+                                margin: EdgeInsets.only(
+                                    bottom:
+                                        MediaQuery.of(context).viewInsets.bottom,
+                                    top: 20,
+                                    left: 10,
+                                    right: 10),
+                                child: Column(
+                                  children: [
+                                    textFormField(
+                                      validate: (value) {
+                                        if (value.toString().isEmpty) {
+                                          return "this filed can't be null";
+                                        }
+                                        return validatePassword(value);
+                                      },
+                                      keyboardType: TextInputType.visiblePassword,
+                                      obscureText: true,
+                                      prefix: Icons.password,
+                                      suffix: Icons.remove_red_eye,
+                                      label: 'enter old password',
+                                      onSaved: (value) {},
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    textFormField(
+                                      validate: (value) {
+                                        if (value.toString().isEmpty) {
+                                          return "this filed can't be null";
+                                        }
+                                        return validatePassword(value);
+                                      },
+                                      keyboardType: TextInputType.visiblePassword,
+                                      obscureText: true,
+                                      prefix: Icons.password,
+                                      suffix: Icons.remove_red_eye,
+                                      label: 'enter new password',
+                                      onSaved: (value) {},
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    textFormField(
+                                      validate: (value) {
+                                        if (value.toString().isEmpty) {
+                                          return "this filed can't be null";
+                                        }
+                                        return validatePassword(value);
+                                      },
+                                      keyboardType: TextInputType.visiblePassword,
+                                      obscureText: true,
+                                      prefix: Icons.password,
+                                      suffix: Icons.remove_red_eye,
+                                      label: 'confirm new password',
+                                      onSaved: (value) {
+
+                                      },
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Spacer(),
+                                        TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text(
+                                              'cancel',
+                                              style: TextStyle(
+                                                  color: Colors.black54),
+                                            )),
+                                        TextButton(
+                                          onPressed: () {
+                                            if (bottomSheetFormKey.currentState!
+                                                .validate()) {
+                                              print('object');
+                                            }
+                                          },
+                                          child: const Text(
+                                            'save',
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                decoration:
+                                                    TextDecoration.underline,
+                                                decorationStyle:
+                                                    TextDecorationStyle.solid,
+                                                decorationColor: Colors.green,
+                                                textBaseline:
+                                                    TextBaseline.alphabetic,
+                                                fontSize: 18),
+                                          ),
                                         ),
-                                      ),
-                                      const Text('camera')
-                                    ],
-                                  ),
-                                  const Spacer(),
-                                  Column(
-                                    children: [
-                                      IconButton(
-                                        onPressed: () async {
-                                          await loadProfileImage();
-                                          Navigator.pop(context);
-                                        },
-                                        icon: const Icon(
-                                          Icons.image,
-                                          color: Colors.blue,
-                                        ),
-                                      ),
-                                      const Text('camera')
-                                    ],
-                                  ),
-                                  const Spacer(),
-                                ],
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           );
                         },
-                        icon: const Icon(Icons.add_a_photo_sharp),
-                        color: Colors.black,
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    name != null ? '$name' : 'name',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  textFormField(
-                    prefix: Icons.person,
-                    label: 'change name',
-                    onSaved: (value) {
-                      setState(() {
-                        name = value;
-                      });
-                    },
-                    validate: (value) {
-                      if (value.toString().isEmpty) {
-                        return 'please enter valid name ';
-                      }
-                    },
-                    keyboardType: TextInputType.text,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  customButton(
-                      text: 'Save changes',
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState!.save();
-                        }
-                      },
-                      width: double.infinity),
-                ],
-              ),
+                        width: double.infinity)
+                  ],
+                ),
+              ],
             ),
           ),
         ),
