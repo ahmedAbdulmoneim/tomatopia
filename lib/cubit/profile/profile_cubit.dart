514c412de355e7bd9c2315d0907fdc99d47195cd
Ahmed <3lbashmohands@gmail.com>
2024-03-26 15:38:14 +0200

    add change username to profile cubit

--- lib/cubit/profile/profile_cubit.dart
+++ lib/cubit/profile/profile_cubit.dart
@@ -6,21 +6,36 @@ import '../../api_models/profile_model.dart';
 import '../../constant/variables.dart';
 import '../../constant/endpints.dart';
 
-class ProfileCubit extends Cubit<ProfileStates>{
-  ProfileCubit(this.tomatopiaServices):super(ProfileInitialState());
+class ProfileCubit extends Cubit<ProfileStates> {
+  ProfileCubit(this.tomatopiaServices) : super(ProfileInitialState());
 
   TomatopiaServices tomatopiaServices;
   ProfileModel? profileModel;
 
-  userData(){
+  userData() {
     emit(ProfileLoadingState());
-    tomatopiaServices.getData(endPoint: profile,token: token).then((value) {
+    tomatopiaServices.getData(endPoint: profile, token: token).then((value) {
       profileModel = ProfileModel.fromJson(value.data);
       print(profileModel!.email);
       emit(ProfileSuccessState());
-    }).catchError((onError){
+    }).catchError((onError) {
       print('profile error $onError');
       emit(ProfileFailureState());
+    });
+  }
+
+  changeUserName({required String newName}) {
+    emit(ChangeNameLoadingState());
+    tomatopiaServices.update(
+        endPoint: changeName,
+        token: token,
+        query: {"NewName": newName}).then((value) {
+      profileModel = ProfileModel.fromJson(value.data);
+      print(profileModel!.name);
+      emit(ChangeNameSuccessState());
+    }).catchError((onError) {
+      print('change name error $onError');
+      emit(ChangeNameFailureState());
     });
   }
 }
--- lib/cubit/profile/profile_states.dart
+++ lib/cubit/profile/profile_states.dart
@@ -7,3 +7,9 @@ class ProfileLoadingState extends ProfileStates {}
 class ProfileSuccessState extends ProfileStates {}
 
 class ProfileFailureState extends ProfileStates {}
+
+class ChangeNameLoadingState extends ProfileStates {}
+
+class ChangeNameSuccessState extends ProfileStates {}
+
+class ChangeNameFailureState extends ProfileStates {}
