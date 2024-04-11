a80812806a516c62632238ee3ae4fef68629bc70
Ahmed <3lbashmohands@gmail.com>
2024-04-11 14:48:37 +0200

    add delete model

---
+++ lib/api_models/delete_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+class DeleteModel{
+  int statusCode ;
+  String message ;
+
+  DeleteModel({required this.message,required this.statusCode});
+  factory DeleteModel.fromJson(Map<String,dynamic> json){
+    return DeleteModel(
+        statusCode: json['stustseCode'],
+        message: json['message']
+    );
+  }
+}
