ab85907588d005b887867010a2e9e21ce9885862
Ahmed <3lbashmohands@gmail.com>
2024-04-13 10:56:58 +0200

    update admin panel

--- lib/admin/admin_panel.dart
+++ lib/admin/admin_panel.dart
@@ -1,9 +1,10 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:tomatopia/admin/category.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tomatopia/admin/disease.dart';
 import 'package:tomatopia/admin/tips.dart';
 import 'package:tomatopia/admin/users.dart';
+import 'package:tomatopia/cubit/admin_cubit/categories_cubit/category_cubit.dart';
+import 'package:tomatopia/cubit/admin_cubit/categories_cubit/category_states.dart';
 import 'package:tomatopia/custom_widget/admin_panel_container.dart';
 
 class AdminPanel extends StatelessWidget {
@@ -20,19 +21,52 @@ class AdminPanel extends StatelessWidget {
         child: ListView(
           children: [
             Image.asset('assets/admin.png'),
-            containerPanel(context, Users(), 'users'),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Users(),
+                      ));
+                },
+                child: containerPanel('users')),
             const SizedBox(
               height: 15,
             ),
-            containerPanel(context, Category(), 'categories'),
+            BlocBuilder<CategoryCubit, CategoryStates>(
+              builder: (context, state) {
+                return GestureDetector(
+                    onTap: () {
+                      BlocProvider.of<CategoryCubit>(context)
+                          .getAllCategories();
+                    },
+                    child: containerPanel('categories'));
+              },
+            ),
             const SizedBox(
               height: 15,
             ),
-            containerPanel(context, Tips(), 'tips'),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Tips(),
+                      ));
+                },
+                child: containerPanel('tips')),
             const SizedBox(
               height: 15,
             ),
-            containerPanel(context, Disease(), 'disease'),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Disease(),
+                      ));
+                },
+                child: containerPanel('disease')),
           ],
         ),
       ),
